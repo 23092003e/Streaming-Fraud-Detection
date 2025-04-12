e0fedb137beab1e1f31633f8c669d3b821aa5609
Hoang Manh <hoangvanmanh2309@gmail.com>
2025-04-12 17:58:56 +0700

    Update

--- src/kafka/kafka_producer.py
+++ src/kafka/kafka_producer.py
@@ -1,33 +1,38 @@
-import os
-import time
-import pandas as pd
 from kafka import KafkaProducer
+import pandas as pd
 import json
+import time
+import random
 
-BOOTSTRAP_SERVERS = os.getenv("BOOTSTRAP_SERVERS", "localhost:9092")
-TOPIC_NAME = os.getenv("TOPIC_NAME", "my_topic")
+def produce_messages(producer, topic, dataframe):
+    time.sleep(5)
+    for _, row in dataframe.iterrows():
+        message = {
+            'transaction_id': int(row['transaction_id']),
+            'type': row['type'],
+            'amount': float(row['amount']),
+            'oldbalanceOrg': float(row['oldbalanceOrg']),
+            'newbalanceOrig': float(row['newbalanceOrig']),
+            'oldbalanceDest': float(row['oldbalanceDest']),
+            'newbalanceDest': float(row['newbalanceDest']),
+        }
 
-def read_data_from_csv(file_path):
-    df = pd.read_csv(file_path)
-    return df
+        producer.send(topic, json.dumps(message).encode('utf-8'))
+        time.sleep(5)
 
-def main():
-    producer = KafkaProducer(
-        bootstrap_servers=BOOTSTRAP_SERVERS,
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
+if __name__=="__main__":
+    # Kafka producer instance
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    kafka_topic = 'financial_transactions'
 
-    data = read_data_from_csv('clean_train.csv')
-    
-    # Giả sử ta sẽ gửi từng dòng lên Kafka, mô phỏng real-time
-    for idx, row in data.iterrows():
-        message = row.to_dict()  # Chuyển row thành dict
-        producer.send(TOPIC_NAME, value=message)
-        print(f"Sent: {message}")
-        time.sleep(1)  # Mô phỏng chờ 1s rồi gửi tiếp
-    
-    producer.flush()
+    # Read data from CSV file
+    csv_file_path = '/home/abdou/Desktop/fraud_detection/Fraud.csv'
+    sample_size = 6000
+
+    # Use pandas to read a random sample of rows
+    df = pd.read_csv(csv_file_path, skiprows=lambda i: i > 0 and random.random() > sample_size / 6362620)
+    df['transaction_id'] = range(1, len(df) + 1)
+
+    produce_messages(producer, kafka_topic, df)
+
     producer.close()
-
-if __name__ == "__main__":
-    main()
