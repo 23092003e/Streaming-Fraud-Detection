b62d2ac448774308c532d8bc5359b1b2322e529c
Hoang Manh <hoangvanmanh2309@gmail.com>
2025-04-13 09:24:18 +0700

    Update

--- src/kafka/kafka_producer.py
+++ src/kafka/kafka_producer.py
@@ -1,38 +1,53 @@
 from kafka import KafkaProducer
-import pandas as pd
 import json
-import time
 import random
+import pandas as pd
+import threading
+import time
 
-def produce_messages(producer, topic, dataframe):
-    time.sleep(5)
-    for _, row in dataframe.iterrows():
-        message = {
-            'transaction_id': int(row['transaction_id']),
-            'type': row['type'],
-            'amount': float(row['amount']),
-            'oldbalanceOrg': float(row['oldbalanceOrg']),
-            'newbalanceOrig': float(row['newbalanceOrig']),
-            'oldbalanceDest': float(row['oldbalanceDest']),
-            'newbalanceDest': float(row['newbalanceDest']),
-        }
-
-        producer.send(topic, json.dumps(message).encode('utf-8'))
-        time.sleep(5)
-
-if __name__=="__main__":
-    # Kafka producer instance
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-    kafka_topic = 'financial_transactions'
-
-    # Read data from CSV file
-    csv_file_path = '/home/abdou/Desktop/fraud_detection/Fraud.csv'
-    sample_size = 6000
-
-    # Use pandas to read a random sample of rows
-    df = pd.read_csv(csv_file_path, skiprows=lambda i: i > 0 and random.random() > sample_size / 6362620)
-    df['transaction_id'] = range(1, len(df) + 1)
-
-    produce_messages(producer, kafka_topic, df)
-
-    producer.close()
+class DataGenerator:
+    index = 0
+    def __init__(self):
+        # read from csv
+        test_data_path = r'./data/raw/fraudTest.csv'
+        self.df = pd.read_csv(test_data_path, index_col=0)
+        print(self.df.columns)
+        
+    def generateTransactions(self):
+        num = random.randint(1, 10)
+        # num = 1
+        messages = self.df[self.index: self.index + num].to_dict(orient='records')
+        self.index += num
+        return messages
+    
+class MyProducer:
+    topic_name = 'transaction_data'
+    
+    def __init__(self):
+        self.producer = KafkaProducer(
+            boostrap_servers = ['localhost:9092'],
+            client_id = 'my-producer',
+            acks = 1,
+            retries = 5,
+            key_serializer = lambda x: json.dumps(x).encode('utf-8'),
+            value_serializer = lambda y: json.dumps(y).encode('utf-8')
+        )
+    
+    def produce_message(self, message):
+        # send the message to the topic
+        self.producer.send(self.topic_name, key=message['transactionId'], value=message)
+        print(f"Produced message: {message}")
+        
+    def send_data(self, message, multi = True):
+        if multi:
+            threads = []
+            for msg in message:
+                thread = threading.Thread(target=self.produce_message, args=(msg,))
+                threads.append(thread)
+                thread.start()
+            for thread in threads:
+                thread.join()
+                
+        else:   
+            future = self.producer.send(self.topic_name, key=message['transactionId'], value=message)
+            
