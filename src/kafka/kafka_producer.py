f2f89e4e2c960870ef5735cd7fc1aa4388ec423c
Hoang Manh <hoangvanmanh2309@gmail.com>
2025-04-13 11:18:04 +0700

    Update

--- src/kafka/kafka_producer.py
+++ src/kafka/kafka_producer.py
@@ -1,70 +1,32 @@
 from kafka import KafkaProducer
 import json
-import random
 import pandas as pd
-import threading
 import time
+import os
 
 class DataGenerator:
-    index = 0
     def __init__(self):
-        # read from csv
-        test_path = r'D:\Data Science\Big Data Technology\Project\Streaming-Fraud-Detection\Streaming-Fraud-Detection\data\raw\fraudTest.csv'
-        self.df = pd.read_csv(test_path, index_col=0)
-        print(self.df.columns)
-        
-    def generateTransactions(self):
-        num = random.randint(1, 10)
-        # num = 1
-        messages = self.df[self.index: self.index + num].to_dict(orient='records')
-        self.index += num
-        return messages
+        test_path = os.path.join("data", "raw", "fraudTest.csv")
+        self.df = pd.read_csv(test_path)
+        self.df.drop(columns=self.df.columns[0], inplace=True) 
 
+    def get_data(self):
+        for _, row in self.df.iterrows():
+            yield row.to_dict()
 
-class MyProducer:
-    topic_name = 'transaction_data'
-    
-    def __init__(self):
-        self.producer = KafkaProducer(
-            bootstrap_servers=['localhost:9092'],
-            client_id="test-producer",
-            acks=1,
-            retries=5, 
-            key_serializer=lambda a:json.dumps(a).encode('utf-8'),
-            value_serializer=lambda b:json.dumps(b).encode('utf-8')
-        )
-    def produce_message(self, message):
-        self.producer.send(self.topic_name, message)
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
 
-    def send_data(self, messages, multi=True):
-        # Use threading for concurrent message production
-        if multi:
-            threads = []
-            for message in messages:
-                thread = threading.Thread(target=self.produce_message, args=(message,))
-                threads.append(thread)
-                thread.start()
+producer = KafkaProducer(
+    bootstrap_servers=['kafka:9092'],
+    value_serializer=json_serializer
+)
 
-            # Wait for all threads to finish
-            for thread in threads:
-                thread.join()
+if __name__ == "__main__":
+    print("Producer started...")
+    generator = DataGenerator()
 
-        else:
-            future=self.producer.send(self.topic_name, value=message, key=message['txn_id'])
-            self.producer.flush()
-
-    def __del__(self):
-        self.producer.close()
-
-print("Starting app...")
-dataGenerator=DataGenerator()
-MyProducer=MyProducer()
-
-try:
-    while True:
-        temp_data=dataGenerator.generateTransactions()
-        MyProducer.send_data(temp_data)
-        print(f'Sent {len(temp_data)} messages...')
-        time.sleep(3)
-except KeyboardInterrupt:
-    exit()
+    for data in generator.get_data():
+        producer.send("fraud-topic", data)
+        print("Sent:", data)
+        time.sleep(1) 
